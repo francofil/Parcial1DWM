5e4b4cdec0ff68223db41c9ce4b71bd7a21e0278
francofil <francofil03@gmail.com>
2024-09-22 18:45:16 -0300

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
+import Contador from './components/contador'
 
 function App() {
 
   return (
     <>
-      <Card/>
+      <Contador/>
     </>
   )
 }
